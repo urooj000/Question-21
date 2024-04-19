6ea75b4b9536e4f7013a9e427bdbb7089d44d6d8
urooj22 <160038688+urooj000@users.noreply.github.com>
2024-04-19 22:08:58 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,4 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let person:{name:string, fname:string, age:number} = {name: "Urooj", fname: "Sajjad", age: 18}
+console.log(person)
